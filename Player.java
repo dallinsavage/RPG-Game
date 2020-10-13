375bfc29c012fd9574d986c4d2317f4a69e23631
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-10-12 18:48:02 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,7 @@
-  public class Player extends Character {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class Player extends Character {
 	Player() {
 		setExp(0);
 		setGold(0);
@@ -18,4 +21,20 @@
 		setHp(getMaxHp());
 		System.out.println("You feel well rested, Hp restored");
 	}
+	public Circle drawPlayer() {
+		Circle player = new Circle(100, 250, 20);
+		player.setFill(Color.BLUE);
+		player.setStroke(Color.BLUE);
+		return player;
+	}
+	public Player spawnPlayer() {
+		
+		int playerEndurance = 1;
+		int playerArmor = 0;
+		int playerDamage = 1;
+		int playerAttackBonus = 0;
+		int points = 25;
+		return null;
+	}
+	
 }
